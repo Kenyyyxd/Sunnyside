7547ee215eda89b3c7db9bd48a2f32913dc27a3d
Kenyyy <k.acuna1398@gmail.com>
2024-09-30 23:33:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+  const menu = document.querySelector(".menu");
+  menu.style.display = "flex";
+}
+function showSidebar() {
+  const menu = document.querySelector(".menu");
+  menu.style.display = "none";
+}
